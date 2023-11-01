118f60e53b80ec256ed1b3bddb53fb7c2d8e16b8
rggrgr <aman.dyachkovskiy.00@mail.ru>
2023-11-02 01:08:02 +0900

    lesson12

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -1,11 +1,26 @@
 package ru.alishev.springcourse;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class ClassicalMusic implements Music {
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
+    }
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Do my init");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Do my destruct");
     }
 }
--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -2,10 +2,15 @@ package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
     private Music music1;
     private Music music2;
 
@@ -15,11 +20,13 @@ public class MusicPlayer {
         this.music2 = music2;
     }
 
-    // @Autowired
-    // public void setMusic(ClassicalMusic classicalMusic, RockMusic rockMusic){
-    //     this.classicalMusic = classicalMusic;
-    //     this.rockMusic = rockMusic;
-    // }
+    public String getName(){
+        return name;
+    }
+
+    public int getVolume(){
+        return volume;
+    }
 
     public String playMusic(){
         return "Playing: " +  music1.getSong() + ", " + music2.getSong();
