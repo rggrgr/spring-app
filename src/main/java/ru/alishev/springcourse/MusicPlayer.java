54e368386846f502955390030c919ddf72658348
rggrgr <aman.dyachkovskiy.00@mail.ru>
2023-11-01 23:46:48 +0900

    lesson6

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -2,12 +2,32 @@ package ru.alishev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
+    public MusicPlayer(Music music){this.music = music;}
+
+    public MusicPlayer(){}
 
     public void playMusic(){
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setMusic(Music music){
+        this.music = music;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+    public int getVolume(){
+        return volume;
     }
 }
